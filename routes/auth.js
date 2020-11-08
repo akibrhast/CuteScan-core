ee4b299a3e823d43fb982bf07f557074be6caeda
Akib Rhast <akib.rhast@gmail.com>
2020-11-08 16:16:24 -0500

    Move google configs out of auth.js and upload.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,16 @@
 const express = require("express");
 const router = new express.Router();
-const ExpressError = require("../expressError")
+const google_configs = require("../google_configs")
 
-const {google} = require('googleapis');
-const OAuth2Data = require('../credentials.json');
-
-const CLIENT_ID = OAuth2Data.web.client_id;
-const CLIENT_SECRET = OAuth2Data.web.client_secret;
-const REDIRECT_URI = OAuth2Data.web.redirect_uris[0];
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
-);
 
 let authed = false;
 
-const SCOPES =
-  'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile';
+
 
 router.get('/', (req, res, next) => {
     try {
         if (!authed) {
-            let url = oAuth2Client.generateAuthUrl({
-              access_type: 'offline',
-              scope: SCOPES,
-            });
-            res.render('index', {url:url});    
+            res.render('index', {url:google_configs.auth_url});    
         }
         else {  
               res.render("success", {
@@ -43,15 +28,14 @@ router.get('/verified', (req, res, next) => {
         const code = req.query.code
         if (code) {
           // Get an access token based on our OAuth code
-          oAuth2Client.getToken(code, function (err, tokens) {
+        
+          google_configs.oAuth2Client.getToken(code, function (err, tokens) {
             if (err) {
               console.log("Error authenticating");
               console.log(err);
             } else {
               console.log("Successfully authenticated");
-              oAuth2Client.setCredentials(tokens);
-              
-      
+              google_configs.oAuth2Client.setCredentials(tokens);
               authed = true;
               res.redirect("/");
             }
@@ -63,7 +47,4 @@ router.get('/verified', (req, res, next) => {
         }      
     });
 
-module.exports = {
-  router:router,
-  oAuth2Client:oAuth2Client
-}
+module.exports = router;
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,58 +1,44 @@
 const express = require('express');
 const router = new express.Router();
-const ExpressError = require('../expressError');
-const mult_config = require('../multerConfig');
-const multer = require("multer");
+
 const fs = require("fs");
-const {google} = require('googleapis');
+const formidable = require('formidable');
+const form = formidable({ multiples: true });
+const google_configs = require("../google_configs")
 
-const OAuth2Data = require('../credentials.json');
-
-const CLIENT_ID = OAuth2Data.web.client_id;
-const CLIENT_SECRET = OAuth2Data.web.client_secret;
-const REDIRECT_URI = OAuth2Data.web.redirect_uris[0];
-
-const auth = require('./auth')
 
 // Don't forgot to make folder uploads in main folder, otherwise you will get an error
 
 router.post("/api/upload_single", (req, res) => {
-    mult_config(req, res, function (err) {       
-      if (err) {
-        console.log(err);
-        return res.end(err);
-      } else {
-        console.log(req.file.path);
-        const drive = google.drive({ 
-            version: "v3",
-            auth: auth.oAuth2Client  
-            });
-        const fileMetadata = {
-          name: req.file.filename,
-        };
-        const media = {
-          mimeType: req.file.mimetype,
-          body: fs.createReadStream(req.file.path),
-        };
-        drive.files.create(
-          {
-            resource: fileMetadata,
-            media: media,
-            fields: "id"
-          },
-          (err, file) => {
-            if (err) {
-              console.error(err);
-            } else {
-              fs.unlinkSync(req.file.path)
-              res.render("success",{success:true})
-            }
-  
-          }
-        );
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    const fileMetadata = {name: files.file.name};
+    const media = {
+      mimeType: files.file.type,
+      body: fs.createReadStream(files.file.path),
+    };
+    google_configs.drive.files.create(
+      {
+        resource: fileMetadata,
+        media: media,
+        fields: "id"
+      },
+      (err, x) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.render("success",{success:true})
+        }
+
       }
-    });
+    );
   });
+
+});
 
 
 // router.post('/api/upload_multiple', mult_config.array('file',10), (req, res, next) => {
